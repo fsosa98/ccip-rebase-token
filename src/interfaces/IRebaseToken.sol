7d8d4275da0f5e925e755260959252dfa6cb7863
fsosa98 <filip.sosa@gmail.com>
2025-03-07 17:51:42 +0100

    Add IRebaseToken

---
+++ src/interfaces/IRebaseToken.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+interface IRebaseToken {
+    function grantMintAndBurnRole(address _account) external;
+
+    function setInterestRate(uint256 _newInterestRate) external;
+
+    function principleBalanceOf(address _user) external view returns (uint256);
+
+    function mint(address _to, uint256 _amount) external;
+
+    function burn(address _from, uint256 _amount) external;
+
+    function transfer(address _to, uint256 _amount) external returns (bool);
+
+    function transferFrom(address _from, address _to, uint256 _amount) external returns (bool);
+
+    function balanceOf(address _user) external view returns (uint256);
+
+    function getUserInterestRate(address _user) external view returns (uint256);
+
+    function getInterestRate() external view returns (uint256);
+}
